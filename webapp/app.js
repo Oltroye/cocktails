f70751943758f52884fa949c5f6a356f5104bdbd
oltroye <stanislas.ibrahim@gmail.com>
2025-03-05 11:31:15 +0100

    merge et fonctionnalité de récupération des données du formulaire d'inscription

--- webapp/app.js
+++ webapp/app.js
@@ -24,6 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/signup', signupRouter);
+app.use('/log_in', indexLogIn);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- webapp/routes/signup.js
+++ webapp/routes/signup.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('signup', { title: 'Signup' });
 });
 
+router.post('/', function(req,res, next) {
+  const { username, name, lastname, password } = req.body;
+  console.log(username, name, lastname, password)
+});
+
 module.exports = router;
